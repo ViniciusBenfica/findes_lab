3119d4c4acfd990e48b93a6fa42ad79ed4b149f0
Vinicius <vinicius.benfica.ruy@gmail.com>
2021-10-18 20:43:09 -0300

    adicionando rota getActivity

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -7,7 +7,22 @@ export const getInfo = (req: Request, res: Response) => {
     fs.readFile(`json/${namejson}.json`, (err, file: Buffer) => {
         if(err) return res.json("Arquivo inexistente")
         const data = JSON.parse(file.toString())
+
         return res.json({"ok": true, "json": data})
     })
+}
 
+export const getMeanActivity = (req: Request, res: Response) => {
+    const { namejson } = req.params
+
+    fs.readFile(`json/${namejson}.json`, (err, arquivo) => {
+        if(err) return res.json("Arquivo inexistente")
+        const data = JSON.parse(arquivo.toString())
+        
+        const result = data.exits.map((item: { exit_name: string , activity: string }) => {
+            return {exit_name : item.exit_name, activity: item.activity}
+        })
+
+        return res.json({"activies": result})
+    })
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { getInfo } from "./controller"
+import { getInfo, getMeanActivity } from "./controller"
 
 const router = Router()
 
 router.get("/jsons.getInfo/:namejson", getInfo)
+router.get("/jsons.getActivity/:namejson", getMeanActivity)
 
 export default router
