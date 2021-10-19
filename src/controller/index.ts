ec5f69015f0663c7b19467671cf30d928adefb10
Vinicius <vinicius.benfica.ruy@gmail.com>
2021-10-19 08:38:23 -0300

    adicionando rota getMeanActivity

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -12,7 +12,7 @@ export const getInfo = (req: Request, res: Response) => {
     })
 }
 
-export const getMeanActivity = (req: Request, res: Response) => {
+export const getActivity = (req: Request, res: Response) => {
     const { namejson } = req.params
 
     fs.readFile(`json/${namejson}.json`, (err, arquivo) => {
@@ -23,6 +23,32 @@ export const getMeanActivity = (req: Request, res: Response) => {
             return {exit_name : item.exit_name, activity: item.activity}
         })
 
-        return res.json({"activies": result})
+        return res.json({"ok": true, "activities": result})
+    })
+}
+
+export const getMeanActivity = (req: Request, res: Response) => {
+    const { namejson } = req.params
+    var count = 0;
+
+    fs.readFile(`json/${namejson}.json`, (err, arquivo) => {
+        if(err) return res.json("Arquivo inexistente")
+        const data = JSON.parse(arquivo.toString())
+        
+        const result = data.exits.map((item: { activity: number }) => {
+            count += item.activity
+            return item.activity
+        })
+
+    var media = count / result.length
+    var mediana = 0
+
+        if(result.length % 2 == 0){
+            mediana = (result[result.length/2] + result[(result.length/2) + 1]) / 2
+        }else{
+            mediana = result[result.length/2]
+        }
+
+        return res.json({"media": media, "mediana": mediana})
     })
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getInfo, getMeanActivity } from "./controller"
+import { getInfo, getActivity, getMeanActivity } from "./controller"
 
 const router = Router()
 
 router.get("/jsons.getInfo/:namejson", getInfo)
-router.get("/jsons.getActivity/:namejson", getMeanActivity)
+router.get("/jsons.getActivity/:namejson", getActivity)
+router.get("/jsons.getMeanActivity/:namejson", getMeanActivity)
 
 export default router
