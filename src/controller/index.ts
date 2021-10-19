00d2503e5b4a5d78bfe6b8c3a4610b91f4847fdb
Vinicius <vinicius.benfica.ruy@gmail.com>
2021-10-19 10:57:59 -0300

    adicionando rota listMeanActivity

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -40,9 +40,9 @@ export const getMeanActivity = (req: Request, res: Response) => {
             return item.activity
         })
 
-    var media = count / result.length
-    var mediana = 0
-
+        var media = count / result.length
+        var mediana = 0
+        
         if(result.length % 2 == 0){
             mediana = (result[result.length/2] + result[(result.length/2) + 1]) / 2
         }else{
@@ -51,4 +51,38 @@ export const getMeanActivity = (req: Request, res: Response) => {
 
         return res.json({"media": media, "mediana": mediana})
     })
+}
+
+export const listMeanActivity = (req: Request, res: Response) =>{    
+    const jsons = fs.readdirSync("json/")
+
+    var result = jsons.map((item) => {
+
+        try{
+            const data = JSON.parse(fs.readFileSync(`json/${item}`).toString())
+            var count = 0, media = 0, mediana = 0
+
+            const activityResult = data.exits.map((item: { activity: number }) => {
+                count += item.activity
+                return item.activity
+            })
+
+            media = count / activityResult.length
+            mediana = 0
+
+            if(activityResult.length % 2 == 0){
+                mediana = (activityResult[activityResult.length/2] + activityResult[(activityResult.length/2) + 1]) / 2
+            }else{
+                mediana = activityResult[activityResult.length/2]
+            }
+
+            return {item, media, mediana}
+
+        }catch(e){
+            return {Error: e}
+        }
+    })
+
+    return res.json(result)
+
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { getInfo, getActivity, getMeanActivity } from "./controller"
+import { getInfo, getActivity, getMeanActivity, listMeanActivity } from "./controller"
 
 const router = Router()
 
 router.get("/jsons.getInfo/:namejson", getInfo)
 router.get("/jsons.getActivity/:namejson", getActivity)
 router.get("/jsons.getMeanActivity/:namejson", getMeanActivity)
+router.get("/jsons.listMeanActivity", listMeanActivity)
 
 export default router
